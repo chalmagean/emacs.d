ecd01756085c9ae2fc154c4fe7e6a2a5c15af731
Cezar Halmagean <cezar@halmagean.ro>
2013-07-03 17:55:54 +0300

    Configured evil mostly

--- init.el
+++ init.el
@@ -19,6 +19,9 @@
 ;; Default tabs should be 2 spaces
 (setq-default tab-width 2)
 
+;; Highlight parenthesis
+(show-paren-mode 1)
+
 ;; Save point position between sessions
 (require 'saveplace)
 (setq-default save-place t)
@@ -28,9 +31,6 @@
 (load "~/.emacs.d/my-dired")
 ;; Magit
 (load "~/.emacs.d/my-magit")
-;; Evil
-(require 'evil)
-(evil-mode 1)
 
 ;; Make CMD work like ALT (on the Mac)
 (setq mac-command-modifier 'meta)
@@ -50,6 +50,81 @@
 ;; Disabling the toolbar
 (tool-bar-mode 0)
 
+;; IBuffer
+(setq ibuffer-formats
+      '((mark modified read-only " "
+              (name 50 50 :left :elide) " "
+              filename-and-process)
+        (mark " " (name 16 -1) " " filename)))
+
+(setq ibuffer-show-empty-filter-groups nil)
+
+;; Uniquify buffers
+(require 'uniquify)
+(setq
+  uniquify-buffer-name-style 'post-forward
+  uniquify-separator " : ")
+
+;; Ruby
+;; Folding
+(add-to-list 'hs-special-modes-alist
+             '(ruby-mode
+               "\\(class\\|def\\|do\\|if\\)" "\\(end\\)" "#"
+               (lambda (arg) (ruby-end-of-block)) nil))
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (hs-minor-mode 1) ;; Enables folding
+            (modify-syntax-entry ?: "."))) ;; Adds ":" to the word definition
+
+;; Bind YARI to C-h R
+(define-key 'help-command "R" 'yari)
+
+;; Undo tree
+(require 'undo-tree)
+(global-undo-tree-mode 1)
+
+;; Ack
+;; Always prompt for a directory root
+(setq ack-and-a-half-prompt-for-directory t)
+(setq ack-and-a-half-executable "/usr/local/bin/ack")
+
+;; Evil
+(require 'evil)
+(evil-mode 1)
+(define-key evil-insert-state-map "k" #'cofi/maybe-exit)
+(setq evil-shift-width 2)
+(setq evil-want-C-u-scroll t)
+ 
+(evil-define-command cofi/maybe-exit ()
+  :repeat change
+  (interactive)
+  (let ((modified (buffer-modified-p)))
+    (insert "k")
+    (let ((evt (read-event (format "Insert %c to exit insert state" ?j)
+               nil 0.5)))
+      (cond
+       ((null evt) (message ""))
+       ((and (integerp evt) (char-equal evt ?j))
+    (delete-char -1)
+    (set-buffer-modified-p modified)
+    (push 'escape unread-command-events))
+       (t (setq unread-command-events (append unread-command-events
+                          (list evt))))))))
+
 ;; Keys
-(global-set-key (kbd "C-c g") 'magit-status)
-(global-set-key (kbd "C-c j") 'dired-jump)
+(define-key evil-normal-state-map ",w" 'save-buffer) ; save
+(define-key evil-normal-state-map ",a" 'ack-and-a-half)
+(define-key evil-normal-state-map ",g" 'magit-status)
+(define-key evil-normal-state-map "-" 'delete-other-windows)
+(define-key evil-normal-state-map ",d" 'dired-jump)
+(define-key evil-normal-state-map ",," 'evil-buffer)
+(define-key evil-normal-state-map ",b" 'ido-switch-buffer)
+
+;;; esc quits
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
