62577a0e30f2b886f076f39cc6ff0dda694f63db
Cezar Halmagean <cezar@halmagean.ro>
2013-07-08 23:36:15 +0300

    Bringing evil back

--- init.el
+++ init.el
@@ -108,6 +108,50 @@
 ;; Bind YARI to C-h R
 (define-key 'help-command "R" 'yari)
 
+;; Evil
+(require 'evil)
+(setq evil-shift-width 2)
+(setq evil-want-C-i-jump t)
+(setq evil-want-C-u-scroll t)
+(evil-mode 1)
+ 
+(evil-define-command cofi/maybe-exit ()
+  :repeat change
+  (interactive)
+  (let ((modified (buffer-modified-p)))
+    (insert "k")
+    (let ((evt (read-event (format "Insert %c to exit insert state" ?j)
+               nil 0.5)))
+      (cond
+       ((null evt) (message ""))
+       ((and (integerp evt) (char-equal evt ?j))
+    (delete-char -1)
+    (set-buffer-modified-p modified)
+    (push 'escape unread-command-events))
+       (t (setq unread-command-events (append unread-command-events
+                          (list evt))))))))
+
+
+;; Keys
+(define-key evil-insert-state-map "k" #'cofi/maybe-exit)
+(define-key evil-normal-state-map ",w" 'save-buffer) ; save
+(define-key evil-normal-state-map ",a" 'ack-and-a-half)
+(define-key evil-normal-state-map ",g" 'magit-status)
+(define-key evil-normal-state-map ",d" 'dired-jump)
+(define-key evil-normal-state-map ",," 'evil-buffer)
+(define-key evil-normal-state-map ",f" 'find-file)
+(define-key evil-normal-state-map ",b" 'switch-to-buffer)
+(define-key evil-normal-state-map ",B" 'ibuffer)
+
+;;; esc quits
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
 ;; RVM
 (require 'rvm)
 (rvm-use-default)
