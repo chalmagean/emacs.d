b22124b6f0ce4e8f6d42b93df621b8aa9163e7cf
Cezar Halmagean <cezar@halmagean.ro>
2013-07-08 21:21:02 +0300

    Removed evil

--- init.el
+++ init.el
@@ -4,6 +4,9 @@
 ;; Adding folders to the load path
 (add-to-list 'load-path "~/.emacs.d/")
 
+;; Custom themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
 ;; Using MELPA for packages
 (when (>= emacs-major-version 24)
   (require 'package)
@@ -14,6 +17,23 @@
 ;; Recompile lisp files when changed
 (require 'auto-compile)
 (auto-compile-on-save-mode 1)
+
+;; Showing whitespace
+(setq whitespace-style (quote (face tabs spaces trailing space-before-tab newline indentation empty space-after-tab space-mark tab-mark newline-mark)))
+(setq whitespace-display-mappings
+      ;; all numbers are Unicode codepoint in decimal. e.g. (insert-char 182 1)
+      '(
+        (space-mark nil) ; 32 SPACE, 183 MIDDLE DOT
+        (newline-mark 10 [172 10]) ; 10 LINE FEED
+        (tab-mark 9 [183 9] [92 9]) ; 9 TAB, MIDDLE DOT
+        ))
+(global-whitespace-mode -1)
+
+;; Setting a default line-height
+(setq-default line-spacing 1)
+
+;; Make _ parts of the "word"
+(modify-syntax-entry ?_ "w")
 
 ;; Don't save any backup files in the current directory
 (setq backup-directory-alist `(("." . "~/.emacs_backups")))
@@ -40,7 +60,7 @@
 (setq mac-command-modifier 'meta)
 
 ;; Choosing a dark theme
-(load-theme 'tango-dark)
+(load-theme 'fogus)
 
 ;; Show line numbers only in opened files
 ;; Another option could be: http://www.emacswiki.org/emacs/linum-off.el
@@ -99,6 +119,9 @@
 ;; Rspec
 (require 'rspec-mode)
 
+;; Rinari
+(require 'rinari)
+(global-rinari-mode)
 
 ;; Undo tree
 (require 'undo-tree)
@@ -109,61 +132,27 @@
 (setq ack-and-a-half-prompt-for-directory t)
 (setq ack-and-a-half-executable "/usr/local/bin/ack")
 
-;; Evil
-(require 'evil)
-(setq evil-shift-width 2)
-(setq evil-want-C-i-jump t)
-(setq evil-want-C-u-scroll t)
-(evil-mode 1)
- 
-(evil-define-command cofi/maybe-exit ()
-  :repeat change
-  (interactive)
-  (let ((modified (buffer-modified-p)))
-    (insert "k")
-    (let ((evt (read-event (format "Insert %c to exit insert state" ?j)
-               nil 0.5)))
-      (cond
-       ((null evt) (message ""))
-       ((and (integerp evt) (char-equal evt ?j))
-    (delete-char -1)
-    (set-buffer-modified-p modified)
-    (push 'escape unread-command-events))
-       (t (setq unread-command-events (append unread-command-events
-                          (list evt))))))))
-
 (defun open-emacs-init-file()
   "Opens the init.el file"
   (interactive)
   (find-file (expand-file-name "init.el" user-emacs-directory)))
-
-;; Keys
-(define-key evil-insert-state-map "k" #'cofi/maybe-exit)
-(define-key evil-normal-state-map ",w" 'save-buffer) ; save
-(define-key evil-normal-state-map ",a" 'ack-and-a-half)
-(define-key evil-normal-state-map ",g" 'magit-status)
-(define-key evil-normal-state-map ",d" 'dired-jump)
-(define-key evil-normal-state-map ",," 'evil-buffer)
-(define-key evil-normal-state-map ",f" 'find-file)
-(define-key evil-normal-state-map ",b" 'switch-to-buffer)
-(define-key evil-normal-state-map ",B" 'ibuffer)
                                           
 (global-set-key (kbd "<f1>") 'open-emacs-init-file)
+(global-set-key (kbd "C-c a") 'ack-and-a-half)
+(global-set-key (kbd "C-c b") 'switch-to-buffer)
+(global-set-key (kbd "C-c B") 'ibuffer)
+(global-set-key (kbd "C-c d") 'dired-jump)
+(global-set-key (kbd "C-c g") 'magit-status)
 
-;;; esc quits
-(define-key evil-normal-state-map [escape] 'keyboard-quit)
-(define-key evil-visual-state-map [escape] 'keyboard-quit)
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("9bac44c2b4dfbb723906b8c491ec06801feb57aa60448d047dbfdbd1a8650897" "f41fd682a3cd1e16796068a2ca96e82cfd274e58b978156da0acce4d56f2b0d5" default))))
+ '(custom-safe-themes (quote ("73abbe794b6467bbf6a9f04867da0befa604a072b38012039e8c1ba730e5f7a5" "af9761c65a81bd14ee3f32bc2ffc966000f57e0c9d31e392bc011504674c07d6" "a4f8d45297894ffdd98738551505a336a7b3096605b467da83fae00f53b13f01" "8eaa3bce3c618cd81a318fcf2d28c1cd21278531f028feb53186f6387547dfb4" "9bac44c2b4dfbb723906b8c491ec06801feb57aa60448d047dbfdbd1a8650897" "f41fd682a3cd1e16796068a2ca96e82cfd274e58b978156da0acce4d56f2b0d5" default)))
+ '(debug-on-error t)
+ '(feature-cucumber-command "cucumber {options} {feature}")
+ '(rspec-use-rake-when-possible nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
